589b36f4db9718cd3f2ab8ca1aa0cf415431d7cd
Robert Tate <btate712@gmail.com>
2019-03-05 03:41:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,3 +35,7 @@ def get_english_meaning(file_path, emoticon)
   end
   return_meaning
 end
+
+puts "Enter an English emoticon:"
+input = gets.chomp
+puts get_japanese_emoticon("emoticons.yml", input)
